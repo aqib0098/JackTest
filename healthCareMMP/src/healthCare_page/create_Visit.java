af62a37a1ce3514738bad7bb10c32bc8734e1759
Aqib <Aqib@DESKTOP-PLJ6NSN.fios-router.home>
2017-12-27 17:13:59 -0600

    test

---
+++ healthCareMMP/src/healthCare_page/create_Visit.java
@@ -0,0 +1,29 @@
+package healthCare_page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class create_Visit {
+	
+
+	WebDriver driver;
+
+	public create_Visit(WebDriver driver)
+	{
+	this. driver = driver;
+	}
+
+	By createVisit=By.xpath("//*[@id='container_body']/p[1]/a/input");
+
+	public  WebElement createVisit() {
+		
+		return driver.findElement(createVisit);
+		
+		// this just a test 
+		// this test2
+		// This test 3
+
+	}	
+
+	}
